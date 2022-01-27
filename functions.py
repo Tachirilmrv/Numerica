27263d299cb216d15f33da87a76bf51c577722cf
jorgehaidar <jorgehaidarmtnez@gmail.com>
2022-01-27 11:36:59 -0500

    add newton y biseccion

--- functions.py
+++ functions.py
@@ -1,9 +1,22 @@
+#-*- coding: utf-8 -*-
+'''
+Busqueda de raices
+Este modulo contiene metodos para la busqueda de raices de ecuaciones de la forma f(x)=0, con f funcion real de variable real, continua y de derivada continua
+'''
+
+
 import re as r
 import math
+import numpy as np
 
 def coefficients(input):
     '''
-    Recibe un polinomio escrito de manera natural y devuelve una lista de float con los coeficientes ordenados segun el grado y con el de mayor grado positivo
+    Argumento
+    input - recibe un polinomio escrito de manera natural
+    
+    Devuelve
+    Una lista de float con los coeficientes del polinomio de manera ordenada
+
     Ejemplo:
 
     \nInput:\n
@@ -43,8 +56,12 @@ def coefficients(input):
 def descartes(values):
     '''
     Aplica la regla de descartes para hallar la mayor cantidad de raices del polinomio
-    Recibe una lista con los coeficientes
-    Devuelve el numero maximo de raices
+    
+    Argumentos
+    values - lista de los valores de los coeficientes
+    
+    Devuelve
+    Cantidad de raices del polinomio
     '''
     value = values[0]
     count = 0
@@ -71,3 +88,61 @@ def find_roots(coef):
     Devuelve True si el polinomio tiene al menos 1 raiz
     '''
     return descartes(coef) != 0
+
+def biseccion(f, a, b, tol=1.0e-6):
+    """
+    Halla una raíz de la función f en el intervalo [a, b] mediante el método de bisección.
+    
+    Argumentos\n
+    f - Función, debe ser tal que f(a) f(b) &lt; 0\n
+    a - Extremo inferior del intervalo\n
+    b - Extremo superior del intervalo\n
+    tol (opcional) - Cota para el error absoluto de la x
+
+    Devuelve
+    x - Raíz de f en [a, b]
+    """
+    if a > b:
+        raise ValueError("Intervalo mal definido")
+    if f(a) * f(b) >= 0.0:
+        raise ValueError("La función debe cambiar de signo en el intervalo")
+    if tol <= 0:
+        raise ValueError("La cota de error debe ser un número positivo")
+    x = (a + b) / 2.0
+    while True:
+        if b - a < tol:
+            return x
+        elif np.sign(f(a)) * np.sign(f(x)) > 0:
+            a = x
+        else:
+            b = x
+        x = (a + b) / 2.0
+
+def newton(f, df, x_0, maxiter=50, xtol=1.0e-6, ftol=1.0e-6):
+    """
+    Halla la raíz de la función f en el entorno de x_0 mediante el método de
+    Newton.\n
+    Argumentos:\n
+    f - Función\n
+    df - Función, debe ser la función derivada de f\n
+    x_0 - Punto de partida del método\n
+    maxiter (opcional) - Número máximo de iteraciones\n
+    xtol (opcional) - Cota para el error relativo para la raíz\n
+    ftol (opcional) - Cota para el valor de la función\n
+    Devuelve:\n
+    x - Raíz de la ecuación en el entorno de x_0
+    Excepciones
+    -----------
+    RuntimeError - No hubo convergencia superado el número máximo de
+iteraciones
+    ZeroDivisionError - La derivada se anuló en algún punto
+    Exception - El valor de x se sale del dominio de definición de f
+    """
+    x = float(x_0) # Se convierte a número de coma flotante
+    for i in range(maxiter):
+        dx = -f(x) / df(x) # ¡Aquí se puede producir una división por cero!
+        # También x puede haber quedado fuera del dominio
+        x = x + dx
+        if abs(dx / x) < xtol and abs(f(x)) < ftol:
+            return x
+    raise RuntimeError("No hubo convergencia después de {} iteraciones").format(maxiter)
