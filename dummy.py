d279cb11b50451791869f7733d2f7ddaff84f9aa
Tachirilmrv <tachirirv@gmail.com>
2022-03-01 13:02:30 -0500

    Añadida función print. Comienzo del trabajo con polinomios en la forma estándar

--- dummy.py
+++ dummy.py
@@ -4,13 +4,16 @@ import numpy.polynomial as pol
 
 
 
-a = [1, 0, 0, 0, 0]
-b = a[::-1]
+a = [1, 2, 3, 4, 5]
+#b = a[::-1]
 
-p1 = pol.Polynomial (b)
+p1 = pol.Polynomial (a)
 
 x = np.linspace(-10, 10, 500)
 y = p1(x)
+
+print(p1)
+print(p1.deriv())
 
 plt.plot (x, y)
 plt.grid ()
